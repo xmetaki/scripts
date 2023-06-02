66d0d2242967b1ecd4c64091248caa2132dcd2a7
Toney Langworth <84306314+xmetaki@users.noreply.github.com>
2023-06-02 17:39:23 +0800

    Create monorepo.sh
    feat: add shell script for creating monorepo fe project more conveniently

---
+++ monorepo.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#git初始化
+if [ ! -d .git ]; then
+    git init
+fi
+#初始化.gitignore
+cat > .gitignore <<EOF
+node_modules
+monorepo.sh
+EOF
+#初始化项目
+pnpm init
+pnpm create @eslint/config
+cat > pnpm-workspace.yaml <<EOF
+packages:
+  - packages/*
+EOF
+pnpm add json husky @commitlint/{config-conventional,cli} lint-staged -w -D
+pnpm exec json -I -f package.json -e 'this.private = true'
+pnpm exec json -I -f package.json -e 'this.scripts["prepare"] = "husky install"'
+pnpm exec json -I -f package.json -e 'this["lint-staged"] = {"*.{ts,tsx,js,vue}": "pnpm exec eslint --fix"}'
+pnpm run prepare
+pnpm exec husky add .husky/commit-msg 'pnpm exec commitlint --edit "$1"'
+pnpm exec husky add .husky/pre-commit 'pnpm exec lint-staged'
+
+cat > commitlint.config.js <<EOF
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: []
+}
+EOF
+touch .npmrc .nvmrc
+mkdir -p packages
